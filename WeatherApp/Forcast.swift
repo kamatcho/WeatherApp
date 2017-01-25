6fb939fb498b34822773ebd7634b29271c3bed1f
MOHAMED <mohamed@MOHAMEDs-MacBook-Pro.local>
2017-01-25 15:50:21 +0200

    First Commit

---
+++ WeatherApp/Forcast.swift
@@ -0,0 +1,9 @@
+//
+//  Forcast.swift
+//  WeatherApp
+//
+//  Created by MOHAMED on 1/22/17.
+//  Copyright © 2017 MOHAMED. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  WeatherApp
+//
+//  Created by MOHAMED on 1/25/17.
+//  Copyright © 2017 MOHAMED. All rights reserved.
+//
+
+import Foundation
